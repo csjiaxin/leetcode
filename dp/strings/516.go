278ff8460f09dcd3cb4cde99c20cea2bc9727689
jliu5 <jliu5@atlassian.com>
2022-07-30 15:31:37 +1000

    strings

--- dp/strings/516.go
+++ dp/strings/516.go
@@ -8,21 +8,44 @@ func max(a, b int) int {
 	}
 	return b
 }
-
-var ret int
-
-func dfs(s string, i, j int) {
-	for ; i >= 0 && j < len(s) && s[i] == s[j]; i, j = i-1, j+1 {
-		ret = max(ret, j-i+1)
+func min(a, b int) int {
+	if a > b {
+		return b
 	}
+	return a
+}
+
+func LCS(text1, text2 string) int {
+	n1 := len(text1)
+	n2 := len(text2)
+
+	dp := make([][]int, n1+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, n2+1)
+	}
+	for i := 1; i <= n1; i++ {
+		for j := 1; j <= n2; j++ {
+			_i := i - 1
+			_j := j - 1
+			if text1[_i] == text2[_j] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			}
+			dp[i][j] = max(dp[i][j], dp[i-1][j])
+			dp[i][j] = max(dp[i][j], dp[i][j-1])
+		}
+	}
+	return dp[n1][n2]
+}
+func reverse(s string) string {
+	b := []byte(s)
+	for i, j := 0, len(s)-1; i <= j; i, j = i+1, j-1 {
+		b[i], b[j] = b[j], b[i]
+	}
+	return string(b)
 }
 func longestPalindromeSubseq(s string) int {
-	ret = 0
-	for i := 0; i < len(s); i++ {
-		dfs(s, i, i)
-		dfs(s, i, i+1)
-	}
-	return ret
+	s2 := reverse(s)
+	return LCS(s, s2)
 }
 
 func main() {
--- dp/strings/readme.md
+++ dp/strings/readme.md
@@ -2,4 +2,8 @@
 
 https://leetcode.com/list/55afh7m7/
 647. Palindromic Substrings
+
+1092. Shortest Common Supersequence
+TODO
 516. Longest Palindromic Subsequence
+https://leetcode.com/problems/longest-palindromic-subsequence/
